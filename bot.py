677c65b5f9e439f164341c3eeea73d56b359df04
donkevlar <kevin.cayouette@gmail.com>
2024-02-01 19:54:57 -0500

    update

--- bot.py
+++ bot.py
@@ -4,17 +4,36 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 from datetime import datetime
-import docker_commands as c
 
+# Other Files
+import docker_commands as c
+import settings
+
+# Load env if not running docker
 if not c.docker_container:
     load_dotenv()
 
 versionNumber = '0.0.1'
 
-c.docker_client_connect()
-
-# Get Discord Token from ENV
-token = os.environ.get("DISCORD_TOKEN")
 
 current_time = datetime.now()
 logging.info(f'\nStarting up bookshelf traveller v.{versionNumber}\n')
+
+# Test Docker Connection
+c.docker_client_connect()
+
+# Discord Bot Setup
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    bot.run(settings.DISCORD_API_SECRET, root_logger=True)
