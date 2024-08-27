c23cbd5a60ad384d0d2fc4eed4f3d8867e6125bf
donkevlar <kevin.cayouette@gmail.com>
2024-08-27 10:56:45 -0400

    moved version to settings.py and bumped to 1.0.1

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,7 +1,11 @@
 import os
+import logging
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
+versionNumber = '1.0.1'
+logging.info(f'Starting DockerBot! Version: {versionNumber}')
 
 DISCORD_API_SECRET = os.getenv('DISCORD_API_SECRET')
